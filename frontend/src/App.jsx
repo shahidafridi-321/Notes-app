21d215a6ddc660531eeae3f46e3ad8edf92f547e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-25 22:15:42 +0500

    Remove un-relevent data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import { Phonebook } from "./Phonebook";
 function App() {
-	const persons = [
-		{
-			id: 1,
-			name: "shahid",
-			number: "89742748",
-		},
-		{ id: 2, name: "shaheen", number: "89742748" },
-		{ id: 3, name: "kamran", number: "89742748" },
-	];
-
 	return (
 		<>
-			<Phonebook data={persons} />
+			<Phonebook />
 		</>
 	);
 }
