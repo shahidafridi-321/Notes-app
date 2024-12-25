5922d4e2d4a988c4dbaa27c80243a8f3cb657752
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-25 20:08:40 +0500

    Add id property to persons arrays objects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import { Phonebook } from "./Phonebook";
 function App() {
 	const persons = [
 		{
-			id: "1",
+			id: 1,
 			name: "shahid",
 			number: "89742748",
 		},
-		{ id: "2", name: "shaheen", number: "89742748" },
-		{ id: "3", name: "kamran", number: "89742748" },
+		{ id: 2, name: "shaheen", number: "89742748" },
+		{ id: 3, name: "kamran", number: "89742748" },
 	];
 
 	return (
