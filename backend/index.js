f3966107fecf0e29e8663ad23e979f749c3b3d93
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-26 12:08:56 +0500

    Create api root responded with all the data

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
+const persons = [
+	{
+		id: "3",
+		name: "Dan Abramov",
+		number: "878887788",
+	},
+	{
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
+		id: "4",
+	},
+	{
+		id: "6371",
+		name: "khan",
+		number: "888",
+	},
+];
+
 const express = require("express");
 const app = express();
 
@@ -5,6 +23,12 @@ app.get("/", (resquest, response) => {
 	response.send("<h2>Heloo shahid</h2>");
 });
 
+app.get("/api/persons", (request, response) => {
+	if (!persons) {
+		response.status(404).end();
+	}
+	response.json(persons);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
