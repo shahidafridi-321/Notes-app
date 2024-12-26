e59491bfbd657e3b4ed924a024f75a8cad58b43a
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-26 18:30:58 +0500

    Create PhonebookList component

---
+++ frontend/src/PhonebookList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "./Button";
+export const PhonebookList = ({ items, handleDelete }) => {
+	return (
+		<div>
+			<ul>
+				{items.map((person) => (
+					<li key={person.id}>
+						{person.name} {person.number}{" "}
+						<Button type="button" text="Delete" onClick={handleDelete} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
